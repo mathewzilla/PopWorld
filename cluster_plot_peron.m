389989cfa189130bb7c92b16b13a5e9ae894998e
mathewzilla <mathewhevans@gmail.com>
2018-08-03 12:16:35 +0100

    added CCons for 197522 examples

--- cluster_plot_peron.m
+++ cluster_plot_peron.m
@@ -1,12 +1,51 @@
 %% Code to load and plot peron crcns clustering results
 
+%% Re-generate CCons and save
+
+f = dir('197522_rejection/Rejected*');
+% f(2) = [];
+for i = 1: numel(f) %linspace(2,numel(f),numel(f)/2)
+        
+            % LOAD EVENTS
+            enw = f(i).name;
+            % Load rejection data
+            load(['197522_rejection/',enw]);%'/Users/mathew/work/PopWorld/197522_rejection/Rejected_an197522_2013_02_18_events_sv_1.mat')
+            
+            if Data.Dn > 0
+            % Compute consensus clustering matrix
+            clusterpars.nreps = 50;
+
+            clusterpars.explore = 'explore';  % allow consensus to use more groups than specified by spectral rejection
+
+            W = Data.Asignal_final;
+            P = Data.ExpA(Data.ixSignal_Final,Data.ixSignal_Final); % extract relevant part of null model
+            B = W - P;
+            m = sum(sum(W))/2;  
+            [Connected.QmaxCluster,Connected.Qmax,Connected.ConsCluster,Connected.ConsQ,ctr,~,C] = ...
+                                        ConsensusCommunityDetect(W,P,1+Data.Dn,1+Data.Dn,clusterpars.nreps,[],clusterpars.explore);
+                
+            for iQ = 1:size(C,2)
+                Q(iQ) = computeQ(C(:,iQ),B,m); % compute modularity Q for each clustering
+            end
+            
+            Allowed = (Q > 0);       % only take consensus using Q-positive clusterings...
+            CCons = makeConsensusMatrix(C(:,Allowed));                     
+                                    
+            % Save C and Consensus matrix
+            save(['~/work/PopWorld/197522_rejection/CCons_',enw(10:end),'.mat'],'CCons','C')
+            end
+
+end
+
+
 %% Look at cluster ID for data vs events versions
 % f = dir('example_clustering/*.mat');
-f = dir('Clustering_Results/Clustered_an197522*');
+f = f = dir('197522_rejection/Rejected*'); %dir('Clustering_Results/Clustered_an197522*');
 f(end) = [];
 for i = 1:25 %linspace(2,numel(f),numel(f)/2)
     try
         if strcmp(f(i).name(31:36),'events')
+            % LOAD EVENTS
             enw = f(i).name;
             %     load(['example_clustering/',f(i-1).name]);
             load(['Clustering_Results/',enw]);
@@ -16,31 +55,37 @@ for i = 1:25 %linspace(2,numel(f),numel(f)/2)
             enw_2 = [enw(1:30),'data_s',enw(37:45)];
             
             
-            %     load(['example_clustering/',f(i).name]);
+            
+            
+            % Load data
+            load /Volumes/05/Peron_2015/Peron_ssc_events/an197522/an197522_2013_02_18.mat
+            
+                        %     load(['example_clustering/',f(i).name]);
+            % LOAD CALCIUM
             load(['Clustering_Results/',enw_2]);
             subplot(2,2,2)
             plot(conv(Full.LouvCluster{1}{1},gausswin(15),'same'));
             title('Calcium')
             suptitle(f(i).name)
             
-            % Load data
-            load /Volumes/05/Peron_2015/Peron_ssc_events/an197522/an197522_2013_02_18.mat
-            
-            
-            
-            
-            
         end
     end
 end
+            
+            
+            
+
 
 %% Load typical example data + clustering output
 
 
 % load('/Users/mathew/work/PopWorld/Clustering_Results/Clustered_an197522_2013_03_07_data_s_sv_1.mat')
-load('/Users/mathew/work/PopWorld/Results/Clustered_Peron_example.mat')
-load('/Users/mathew/work/PopWorld/Results/Rejected_Peron_example.mat')
-load('/Volumes/05/Peron_2015/Peron_ssc_events/an197522/an197522_2013_03_07.mat')
+% load('/Users/mathew/work/PopWorld/Results/Clustered_Peron_example.mat')
+% load('/Users/mathew/work/PopWorld/Results/Rejected_Peron_example.mat')
+
+load('/Users/mathew/work/PopWorld/197522_rejection/Rejected_an197522_2013_02_18_events_sv_1.mat')
+load('Clustering_Results/Clustered_an197522_2013_02_18_events_sv_1.mat')
+load('/Volumes/05/Peron_2015/Peron_ssc_events/an197522/an197522_2013_02_18.mat'); % 2013_03_07
 
 %% High skewness neurons
 ca = dat.timeSeriesArrayHash.value{2}.valueMatrix;
